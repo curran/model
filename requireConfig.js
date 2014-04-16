98c1ee26162814efd9b892fc3790daaa973b90a0
curran <curran.kelleher@gmail.com>
2014-04-16 19:40:30 -0400

    Removed old lodash reference from requireConfig

--- requireConfig.js
+++ requireConfig.js
@@ -1,12 +1,6 @@
 // The configuration for Require.js
-// Curran Kelleher 4/15/2014
+// Curran Kelleher 4/16/2014
 require.config({
-
   // Look up modules under the `src` directory.
-  baseUrl: 'src',
-
-  // Map the `_` module name to lib/lodash.
-  paths: {
-    _: '../lib/lodash'
-  }
+  baseUrl: 'src'
 });

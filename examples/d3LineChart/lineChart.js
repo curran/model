6f10f71d8c44f8ba8dde356da26a54c1fb157883
curran <curran.kelleher@gmail.com>
2014-04-17 10:50:16 -0400

    Added README for D3 line chart example

--- examples/d3LineChart/lineChart.js
+++ examples/d3LineChart/lineChart.js
@@ -1,11 +1,11 @@
-// An adaptation of the [D3 bar chart example](http://bl.ocks.org/mbostock/3885304)
+// An adaptation of the [D3 line chart example](http://bl.ocks.org/mbostock/3883245)
 // that uses `model.js`. This version, unlike the original example,
 // is model driven and reactive. When a part of the model updates,
 // only the parts of the visualization that depend on those parts
 // of the model are updated. There are no redundant calls to visualization
 // update code when multiple properties are changed simultaneously.
 //
-// Define the bar chart AMD module using the
+// Define the line chart AMD module using the
 // `define()` function provided by Require.js.
 define(['model'], function (Model) {
   return function BarChart(div){

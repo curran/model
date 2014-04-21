65b555b18559a4f8e1d7f217c64eab6713393fed
curran <curran.kelleher@gmail.com>
2014-04-20 21:13:06 -0400

    Split up enter, update and exit phases for Choropleth

--- examples/d3Choropleth/main.js
+++ examples/d3Choropleth/main.js
@@ -9,9 +9,13 @@ require(['model', 'd3', 'topojson'], function (Model, d3, topojson) {
         .translate([width / 2, height / 2]),
       path = d3.geo.path()
         .projection(projection),
-      svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height),
+      svg = d3.select('body').append('svg')
+        .attr('width', width)
+        .attr('height', height),
+      countiesG = svg.append('g')
+        .attr('class', 'counties'),
+      states = svg.append('path')
+        .attr('class', 'states'),
       model = Model();
 
   d3.json('us.json', function (err, us) {
@@ -27,18 +31,15 @@ require(['model', 'd3', 'topojson'], function (Model, d3, topojson) {
   });
 
   model.when(['us', 'rateById'], function (us, rateById) {
-    svg.append("g")
-        .attr("class", "counties")
-      .selectAll("path")
-        .data(topojson.feature(us, us.objects.counties).features)
-      .enter().append("path")
-        .attr("class", function(d) { return quantize(rateById[d.id]); })
-        .attr("d", path);
+    var counties = countiesG.selectAll('path')
+      .data(topojson.feature(us, us.objects.counties).features);
 
-    svg.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-        .attr("class", "states")
-        .attr("d", path);
+    counties.enter().append('path')
+    counties
+      .attr('class', function(d) { return quantize(rateById[d.id]); })
+      .attr('d', path);
+
+    states.attr('d', path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
   });
 
   d3.select(self.frameElement).style("height", height + "px");

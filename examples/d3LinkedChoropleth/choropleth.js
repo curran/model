b4d5514091c520546380b4a12cedf09567ee3614
curran <curran.kelleher@gmail.com>
2014-04-22 22:00:23 -0400

    Started working on linked choropleth

---
+++ examples/d3LinkedChoropleth/choropleth.js
@@ -0,0 +1,138 @@
+// A dynamic Choropleth map using model.js.
+//
+// Draws from:
+//
+//  * Mike Bostock's Choropleth D3 examples
+//    http://bl.ocks.org/mbostock/4060606
+//    http://bl.ocks.org/mbostock/3757132
+//  * SVG Geometric Zooming example
+//    http://bl.ocks.org/mbostock/3680999
+//  * Merging states using topojson
+//    https://gist.github.com/mbostock/5416405
+//  * Zooming to a feature
+//    https://gist.github.com/mbostock/4699541
+//  * Simple D3 tooltip
+//    https://gist.github.com/biovisualize/1016860
+//  * CSS TEXT SHADOWS
+//    http://www.w3.org/Style/Examples/007/text-shadow.en.html#multiple
+// 
+// By Curran Kelleher 4/22/2014
+define(['model', 'd3', 'topojson'], function (Model, d3, topojson) {
+  return function (div) {
+    var quantize = d3.scale.quantize().domain([0, .15])
+          .range(d3.range(9).map(function(i) { return 'q' + i + '-9'; })),
+        projection = d3.geo.equirectangular().translate([0, 0]).scale(1),
+        path = d3.geo.path().projection(projection),
+        svg = d3.select(div).append('svg').style('position', 'absolute'),
+        g = svg.append('g'),
+        countriesG = g.append('g').attr('class', 'counties'),
+        states = g.append('path').attr('class', 'states'),
+        zoomBehavior = d3.behavior.zoom().on('zoom', function () {
+          model.set({
+            pan: d3.event.translate,
+            zoom: d3.event.scale
+          });
+        }),
+        tooltip = d3.select(div)
+          .append('div')
+          .attr('class', 'tooltip')
+          .style('position', 'absolute')
+          .style('z-index', '10')
+          .style('visibility', 'hidden');
+        model = Model(),
+        format = d3.format(',');
+
+    // Set default pan & zoom
+    model.set({
+      pan: [0, 0],
+      zoom: 1
+    });
+
+    // Set pan & zoom in response to interaction
+    svg.call(zoomBehavior);
+
+    model.when('box', function (box) {
+      svg.attr('width', box.width)
+         .attr('height', box.height);
+      svg.style('left', box.x + 'px')
+        .style('top', box.y + 'px');
+    });
+
+    model.when(['data', 'idField', 'colorField'], function (data, idField, colorField) {
+      var dataById = {};
+      data.forEach(function (d) {
+        dataById[d[idField]] = +d[colorField];
+      });
+      model.set('dataById', dataById);
+    });
+
+    model.when(['countries', 'box'], function (countries, box) {
+      var countryFeatures = topojson.feature(countries, countries.objects.countriesGeoJSON).features,
+ 
+          // Fit the map on the display.
+          worldGeom = topojson.merge(countries, countries.objects.countriesGeoJSON.geometries),
+          bounds = path.bounds(worldGeom),
+          dx = bounds[1][0] - bounds[0][0],
+          dy = bounds[1][1] - bounds[0][1],
+          x = (bounds[0][0] + bounds[1][0]) / 2,
+          y = (bounds[0][1] + bounds[1][1]) / 2,
+          scale = 1 / Math.max(dx / box.width, dy / box.height);
+
+      projection
+        .scale(scale)
+        .translate([ box.width / 2 - scale * x, box.height / 2 - scale * y ]);
+
+      model.set({
+        countryFeatures: countryFeatures,
+
+        // Set countries to null so this function never executes again
+        countries: null
+      });
+    });
+
+    // Update the transform based pan & zoom
+    model.when(['pan', 'zoom'], function (pan, zoom) {
+      g.attr('transform', 'translate(' + pan + ')scale(' + zoom + ')');
+    });
+    
+    // Update the county polygons
+    model.when('countryFeatures', function (countryFeatures) {
+      countries(countryFeatures).attr('d', path)
+        .attr('fill', 'red');
+    });
+
+    // Update the color only (not polygons) when the data changes
+    model.when(['countryFeatures', 'dataById', 'colorField'], function (countryFeatures, dataById, colorField) {
+      countries(countryFeatures)
+        .attr('class', function(d) {
+          if(!dataById[d.id]){
+            console.log(d.id);
+          }
+          return quantize(dataById[d.id]);
+        })
+        .on('mouseover', function (d) {
+          var colorField = model.get('colorField');
+          tooltip
+            .style('visibility', 'visible')
+            .html(d.id + '<br>' + colorField + ': ' + format(Math.round(dataById[d.id] * 1000000000)));
+        })
+        .on('mousemove', function () {
+          tooltip
+            .style('top', (event.pageY - 10) + 'px')
+            .style('left',(event.pageX + 10) + 'px');
+        })
+        .on('mouseout', function () {
+          tooltip.style('visibility', 'hidden');
+        });
+    });
+
+    function countries(countryFeatures){
+      var countries = countriesG.selectAll('path').data(countryFeatures);
+      countries.enter().append('path');
+      return countries;
+    }
+
+    return model;
+  };
+});
+
--- examples/d3LinkedChoropleth/lineChart.js
+++ examples/d3LinkedChoropleth/lineChart.js
@@ -1,21 +1,19 @@
-// An adaptation of the [D3 bar chart example](http://bl.ocks.org/mbostock/3885304)
+// An adaptation of the [D3 line chart example](http://bl.ocks.org/mbostock/3883245)
 // that uses `model.js`. This version, unlike the original example,
 // is model driven and reactive. When a part of the model updates,
 // only the parts of the visualization that depend on those parts
 // of the model are updated. There are no redundant calls to visualization
 // update code when multiple properties are changed simultaneously.
 //
-// Define the bar chart AMD module using the
+// Define the line chart AMD module using the
 // `define()` function provided by Require.js.
 define(['d3', 'model'], function (d3, Model) {
-  return function BarChart(div){
-    var x = d3.scale.ordinal(),
+  return function (div){
+    var x = d3.time.scale(),
         y = d3.scale.linear(),
         xAxis = d3.svg.axis().scale(x).orient('bottom'),
         yAxis = d3.svg.axis().scale(y).orient('left'),
-
-        // Use absolute positioning so that CSS can be used
-        // to position the div according to the model.
+        line = d3.svg.line(),
         svg = d3.select(div).append('svg').style('position', 'absolute'),
         g = svg.append('g'),
         xAxisG = g.append('g').attr('class', 'x axis'),
@@ -27,7 +25,6 @@ define(['d3', 'model'], function (d3, Model) {
           .style('text-anchor', 'end'),
         model = Model();
 
-
     model.when('yLabel', yAxisLabel.text, yAxisLabel);
 
     model.when('margin', function (margin) {
@@ -37,12 +34,7 @@ define(['d3', 'model'], function (d3, Model) {
     model.when('box', function (box) {
       svg.attr('width', box.width)
          .attr('height', box.height);
-
-      // Set the CSS `left` and `top` properties
-      // to move the SVG element to `(box.x, box.y)`
-      // relative to the container div.
-      svg
-        .style('left', box.x + 'px')
+      svg.style('left', box.x + 'px')
         .style('top', box.y + 'px');
     });
 
@@ -55,24 +47,40 @@ define(['d3', 'model'], function (d3, Model) {
       xAxisG.attr('transform', 'translate(0,' + height + ')');
     });
 
-    model.when(['width', 'height', 'data', 'barField', 'heightField'], function (width, height, data, barField, heightField) {
-      var bars;
+    model.when(['width', 'height', 'data', 'xField', 'yField', 'idField'], 
+        function (width, height, data, xField, yField, idField) {
+      var lineData = (function () {
+            var entriesById = {};
+            data.forEach(function (d) {
+              var id = d[idField],
+                  entries = entriesById[id] || (entriesById[id] = []);
+              entries.push(d);
+            });
+            return Object.keys(entriesById).map(function (id) {
+              return {
+                name: id,
+                values: entriesById[id]
+              };
+            });
+          }()),
+          paths;
 
-      x.domain(data.map(function(d) { return d[barField]; }));
-      y.domain([0, d3.max(data, function(d) { return d[heightField]; })]);
-      x.rangeRoundBands([0, width], .1);
+      x.domain(d3.extent(data, function(d) { return d[xField]; }));
+      y.domain(d3.extent(data, function(d) { return d[yField]; }));
+
+      x.range([0, width]);
       y.range([height, 0]);
 
       xAxisG.call(xAxis);
       yAxisG.call(yAxis);
 
-      bars = g.selectAll('.bar').data(data);
-      bars.enter().append('rect').attr('class', 'bar');
-      bars.attr('x', function(d) { return x(d[barField]); })
-          .attr('width', x.rangeBand())
-          .attr('y', function(d) { return y(d[heightField]); })
-          .attr('height', function(d) { return height - y(d[heightField]); });
-      bars.exit().remove();
+      line
+        .x(function(d) { return x(d[xField]); })
+        .y(function(d) { return y(d[yField]); });
+
+      paths = g.selectAll('.line').data(lineData);
+      paths.enter().append('path').attr('class', 'line');
+      paths.attr('d', function(d) { return line(d.values); });
     });
     return model;
   }

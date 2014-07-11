ed1d74d8b3e071b771d745a762fc85a13e0e11b6
curran <curran.kelleher@gmail.com>
2014-07-11 09:43:23 -0400

    Updated proof of concept refactoring to dynamically track properties

--- sandbox/model2.js
+++ sandbox/model2.js
@@ -4,97 +4,92 @@
 //
 // Curran Kelleher 7/10/2014
 
-
-// Queues a function to execute on the 
-// next tick of the JavaScript event loop.
-function debounce(fn){
+function debounce(callback){
   var queued = false;
   return function () {
     if(!queued){
       queued = true;
       setTimeout(function () {
         queued = false;
-        fn();
+        callback();
       }, 0);
     }
   };
 }
 
-// Returns true if every element in the given array
-// is defined (not null or 'undefined').
 function allAreDefined(arr){
   return !arr.some(function (d) {
     return typeof d === 'undefined' || d === null;
   });
 }
 
-// The Model constructor function. Accepts a `values` argument
-// that serves as the default values of the model. Any properties
-// not present in the `values` argument will not be tracked.
-// For use as dependency arguments to `when`, default properties
-// must be present in the `values` object passed to the constructor.
-function Model(values) {
-  var listeners = {};
-  return Object.create({
-    when: function (dependencies, callback) {
-      var listener = debounce(function(){
-        var args = dependencies.map(function (property) {
-          return values[property];
-        });
-        if(allAreDefined(args)) {
-          callback.apply(null, args);
-        }
+function Model() {
+  var listeners = {},
+      trackedProperties = {},
+      values = {},
+      model;
+
+  function setUpListener(properties, callback){
+    var listener = debounce(function(){
+      var args = properties.map(function (property) {
+        return values[property];
       });
-
-      listener();
-
-      // Support both when(['a', 'b'], callback) and when('c', callback).
-      if(!(dependencies instanceof Array)) {
-        dependencies = [dependencies];
+      if(allAreDefined(args)) {
+        callback.apply(null, args);
       }
+    });
+    listener();
+    properties.forEach(function(property){
+      if(!listeners[property]) {
+        listeners[property] = [];
+      }
+      listeners[property].push(listener);
+    });
+  }
 
-      dependencies.forEach(function(property){
-        if(!listeners[property]) {
-          listeners[property] = [];
-        }
-        listeners[property].push(listener);
-      });
+  function setUpProperties(properties){
+    properties.forEach(function(property){
+      if(!trackedProperties[property]) {
+        trackedProperties[property] = true;
+        values[property] = model[property];
+        Object.defineProperty(model, property, {
+          get: function () {
+            return values[property];
+          },
+          set: function(value) {
+            values[property] = value;
+
+            if(listeners[property]){
+              listeners[property].forEach(function (listener) {
+                listener();
+              });
+            }
+          }
+        });
+      }
+    });
+  }
+
+  model = Object.create({
+    when: function (properties, callback) {
+      setUpListener(properties, callback);
+      setUpProperties(properties);
     },
     removeListeners: function () {
-      // Removes reference to all old listeners.
-      // This causes old listeners to be garbage collected.
       listeners = {};
     }
-  }, (function () {
-    var propertiesObject = {};
-    Object.keys(values).forEach(function (property) {
-      propertiesObject[property] = {
-        get: function () {
-          return values[property];
-        },
-        set: function(value) {
-          values[property] = value;
+  });
 
-          if(listeners[property]){
-            listeners[property].forEach(function (listener) {
-              listener();
-            });
-          }
-        }
-      };
-    });
-    return propertiesObject;
-  }()));
+  return model;
 }
 
 // Create a new model.
-var model = Model({
-  x: 5,
-  y: 10
-});
+var model = Model();
+model.x = 5;
+model.y = 10;
 
-model.when(['x'], function (x) {
-  console.log(x);
+model.when(['x', 'y'], function (x, y) {
+  console.log(x, y);
 });
 
 setTimeout(function(){
@@ -104,3 +99,5 @@ setTimeout(function(){
     model.x = 500;
   }, 1000);
 }, 1000);
+
+console.log(JSON.stringify(model, null, 2));

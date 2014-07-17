b97a3d3549011f8bfd2f1b97f99bc35e9249576d
curran <curran.kelleher@gmail.com>
2014-07-16 20:53:12 -0400

    Updated bar chart example to mirror example code from reactiveDataVis talk

--- examples/d3BarChart/barChart.js
+++ examples/d3BarChart/barChart.js
@@ -1,70 +1,75 @@
-// An adaptation of the [D3 bar chart example](http://bl.ocks.org/mbostock/3885304)
-// that uses `model.js`. This version, unlike the original example,
-// is model driven and reactive. When a part of the model updates,
-// only the parts of the visualization that depend on those parts
-// of the model are updated. There are no redundant calls to visualization
-// update code when multiple properties are changed simultaneously.
-//
-// By Curran Kelleher July 2014
-//
-// Define the bar chart AMD module using the
-// `define()` function provided by Require.js.
-define(['d3', 'model'], function (d3, Model) {
-  return function BarChart(div){
-    var x = d3.scale.ordinal(),
-        y = d3.scale.linear(),
-        xAxis = d3.svg.axis().scale(x).orient('bottom'),
-        yAxis = d3.svg.axis().scale(y).orient('left').ticks(10, '%'),
-        svg = d3.select(div).append('svg'),
-        g = svg.append('g'),
-        xAxisG = g.append('g').attr('class', 'x axis'),
-        yAxisG = g.append('g').attr('class', 'y axis'),
-        yAxisLabel = yAxisG.append('text')
-          .attr('transform', 'rotate(-90)')
-          .attr('y', 6)
-          .attr('dy', '.71em')
-          .style('text-anchor', 'end'),
-        model = Model();
+define(["d3", "model"], function (d3, Model) {
+  return function BarChart (container) {
+    var model = Model(),
+        xAxis = d3.svg.axis().orient("bottom"),
+        yAxis = d3.svg.axis().orient("left").ticks(10, "%"),
+        svg = d3.select(container).append("svg"),
+        g = svg.append("g"),
+        xAxisG = g.append("g").attr("class", "x axis"),
+        yAxisG = g.append("g").attr("class", "y axis"),
+        yAxisText = yAxisG.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end");
 
-    model.when('yLabel', yAxisLabel.text, yAxisLabel);
+    model.margin = {top: 20, right: 20, bottom: 30, left: 40};
 
-    model.when('margin', function (margin) {
-      g.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    model.when(["box", "margin"], function (box, margin) {
+      model.width = box.width - margin.left - margin.right,
+      model.height = box.height - margin.top - margin.bottom;
     });
 
-    model.when('size', function (size) {
-      svg.attr('width', size.width)
-         .attr('height', size.height);
+    model.when("box", function (box) {
+      svg.attr("width", box.width).attr("height", box.height);
     });
 
-    model.when(['size', 'margin'], function (size, margin) {
-      model.width = size.width - margin.left - margin.right;
-      model.height = size.height - margin.top - margin.bottom;
+    model.when("margin", function (margin) {
+      g.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     });
 
-    model.when('height', function (height) {
-      xAxisG.attr('transform', 'translate(0,' + height + ')');
+    model.when("height", function (height) {
+      xAxisG.attr("transform", "translate(0," + height + ")");
     });
 
-    model.when(['width', 'height', 'data', 'barField', 'heightField'], function (width, height, data, barField, heightField) {
-      var bars;
+    model.when(["data", "xAttribute", "width"], function (data, xAttribute, width) {
+      model.xScale = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1)
+        .domain(data.map(function(d) { return d[xAttribute]; }));
+    });
 
-      x.domain(data.map(function(d) { return d[barField]; }));
-      y.domain([0, d3.max(data, function(d) { return d[heightField]; })]);
-      x.rangeRoundBands([0, width], .1);
-      y.range([height, 0]);
+    model.when(["data", "yAttribute", "height"], function (data, yAttribute, height) {
+      model.yScale = d3.scale.linear()
+        .range([height, 0])
+        .domain([0, d3.max(data, function(d) { return d[yAttribute]; })]);
+    });
 
+    model.when(["xScale"], function (xScale) {
+      xAxis.scale(xScale)
       xAxisG.call(xAxis);
-      yAxisG.call(yAxis);
+    });
 
-      bars = g.selectAll('.bar').data(data);
-      bars.enter().append('rect').attr('class', 'bar');
-      bars.attr('x', function(d) { return x(d[barField]); })
-          .attr('width', x.rangeBand())
-          .attr('y', function(d) { return y(d[heightField]); })
-          .attr('height', function(d) { return height - y(d[heightField]); });
+    model.when(["yScale"], function (yScale) {
+      yAxis.scale(yScale)
+      yAxisG.call(yAxis);
+    });
+
+    model.when("yAxisLabel", yAxisText.text, yAxisText);
+
+    model.when(["data", "xAttribute", "yAttribute", "xScale", "yScale", "height"],
+        function (data, xAttribute, yAttribute, xScale, yScale, height) {
+      var bars = g.selectAll(".bar").data(data);
+
+      bars.enter().append("rect").attr("class", "bar");
+
+      bars.attr("x", function(d) { return xScale(d[xAttribute]); })
+        .attr("width", xScale.rangeBand())
+        .attr("y", function(d) { return yScale(d[yAttribute]); })
+        .attr("height", function(d) { return height - yScale(d[yAttribute]); });
+
       bars.exit().remove();
     });
+
     return model;
-  }
+  };
 });
--- examples/d3BarChart/main.js
+++ examples/d3BarChart/main.js
@@ -1,70 +1,34 @@
-// The main program that runs the bar chart example.
-// This program creates a new bar chart, loads a
-// configuration file for it, and loads data into it.
-// Then it periodically updates various aspects of the chart
-// with random values, to illustrate that the chart
-// is dynamic and reacts to changes in its model.
-//
-// Curran Kelleher July 2014
-require(['d3', 'barChart'], function (d3, BarChart) {
-  var div = document.getElementById('barChartContainer'),
-      barChart = BarChart(div);
+require(["d3", "barChart"], function (d3, BarChart) {
+  var container = document.getElementById("container"),
+      barChart = BarChart(container);
 
-  d3.json('configuration.json', function (config) {
-    barChart.set(config);
+  d3.tsv("../data/letterByFrequency.tsv", type, function(error, data) {
+    barChart.set({
+      xAttribute: "letter",
+      yAttribute: "frequency",
+      yAxisLabel: "Frequency",
+      data: data
+    });
+    setInterval(function () {
+      barChart.data = data.filter(function(){ return Math.random() < 0.5 });
+    }, 1000);
+    setInterval(function () {
+      var i = Math.floor(Math.random() * 3);
+      barChart.yAxisLabel = ["A", "B", "C"][i];
+    }, 1500);
   });
 
-  d3.tsv('data.tsv', function (d) {
+  function type(d) {
     d.frequency = +d.frequency;
     return d;
-  }, function(error, data) {
+  }
 
-    // Set size once to initialize
-    setSizeFromDiv();
-
-    // Set size on resize
-    window.addEventListener('resize', setSizeFromDiv);
-
-    // Set the data
-    barChart.set.data = data;
-
-    // Reset data each second
-    setInterval(function () {
-
-      // Include each element with a 50% chance.
-      var randomSample = data.filter(function(d){
-        return Math.random() < 0.5;
-      });
-
-      barChart.data = randomSample;
-    }, 1000);
-
-    // Randomly change the margin every 1.7 seconds.
-    function random(){ return Math.random() * 100; }
-    setInterval(function () {
-      barChart.margin = {
-        top: random(),
-        right: random(),
-        bottom: random(),
-        left: random()
-      };
-    }, 1700);
-
-    // Change the Y axis label every 600 ms.
-    function randomString() {
-      var possibilities = ['Frequency', 'Population', 'Alpha', 'Beta'],
-          i = Math.round(Math.random() * possibilities.length);
-      return possibilities[i];
-    }
-    setInterval(function () {
-      barChart.yLabel = randomString();
-    }, 600);
-  });
-
-  function setSizeFromDiv(){
-    barChart.size = {
-      width: div.clientWidth,
-      height: div.clientHeight
+  function computeBox(){
+    barChart.box = {
+      width: container.clientWidth,
+      height: container.clientHeight
     };
   }
+  computeBox();
+  window.addEventListener("resize", computeBox);
 });

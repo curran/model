97219b71707f612819e85d6d4d7a59421b3b88bf
Curran <curran.kelleher@gmail.com>
2014-11-23 13:52:19 -0500

    Added hover interaction between choropleth and line chart.

--- examples/d3LinkedChoropleth/choropleth.js
+++ examples/d3LinkedChoropleth/choropleth.js
@@ -50,12 +50,22 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
             zoom: d3.event.scale
           });
         }),
+        whiteShadow = "-1px -1px 2px #FFF, 1px -1px 2px #FFF, -1px  1px 2px #FFF, 1px  1px 2px #FFF",
         tooltip = d3.select(div)
           .append("div")
-          .attr("class", "tooltip")
           .style("position", "absolute")
+
+          // Make sure the tooltip renders on top of
+          // everything else.
           .style("z-index", "10")
+          .style("font-size", "2em")
+          .style("text-shadow", whiteShadow)
+
+          // Disable pointer events so the tooltip text does not
+          // interfere with the mouseover behavior of geometries while dragging.
+          .style("pointer-events", "none")
           .style("visibility", "hidden"),
+
         model = Model(),
         format = d3.format(","),
         missingDataFill = "url(#diagonalHatch)",
@@ -67,12 +77,14 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
 
     legend
       .append("rect")
-      .attr("x", 20)
+      .attr("x", 20.5)
       .attr("y", function(d, i) {
-         return i * 30 + 20;
+         return i * 30 + 20.5;
       })
      .attr("width", 20)
      .attr("height", 20)
+     .style("stroke", "gray")
+     .style("stroke-width", "1px")
      .attr("class", function(d){ 
        return d === "missing" ? null : d;
      })
@@ -82,12 +94,13 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
 
     legend
       .append("text")
-      .attr("x", 43)
+      .attr("x", 45)
       .attr("y", function(d, i) {
          return i * 30 + 20;
       })
-      .attr("dy", "1.1em")
+      .attr("dy", "1.2em")
       .style("font-size", "1.3em")
+      .style("text-shadow", whiteShadow)
       .text(function(d, i) {
         if(d === "missing") {
           return "Missing data";
@@ -119,6 +132,7 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
         .style("top", box.y + "px");
     });
 
+    // Restructure the data for use on the choropleth map.
     model.when(["data", "idField", "colorField"], function (data, idField, colorField) {
       var dataById = {};
       data.forEach(function (d) {
@@ -127,6 +141,7 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
       model.dataById = dataById;
     });
 
+    // Preprocess the geometries for use in the visualization.
     model.when(["countries", "box"], function (countries, box) {
       var countryFeatures = topojson.feature(countries, countries.objects.countriesGeoJSON).features,
  
@@ -146,19 +161,22 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
       model.set({
         countryFeatures: countryFeatures,
 
-        // Set countries to null so this function never executes again
+        // Set countries to null so this function never executes again.
         countries: null
       });
     });
 
-    // Update the transform based pan & zoom
+    // Update the transform based on pan & zoom.
     model.when(["pan", "zoom"], function (pan, zoom) {
       g.attr("transform", "translate(" + pan + ")scale(" + zoom + ")");
     });
 
-    // Update the county polygons
+    // Update the polygons.
     model.when("countryFeatures", function (countryFeatures) {
       countries(countryFeatures).attr("d", path)
+
+        // Default the fill to the missing data color.
+        // This will be replaced later for all regions that have data.
         .attr("fill", missingDataFill);
     });
 
@@ -177,7 +195,6 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
       }).data();
       model.visibleRegions = visibleRegions;
     });
-    
 
     // Update the color only (not polygons) when the data changes
     model.when(["countryFeatures", "dataById", "colorField"], function (countryFeatures, dataById, colorField) {
@@ -188,6 +205,16 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
         .on("mouseover", function (d) {
           var html = d.id + "<br>" + colorField + ": " + format(Math.round(dataById[d.id] * 1000000000));
           tooltip.style("visibility", "visible").html(html);
+
+          // Highlight the hovered geometry with the same color
+          // used to highlight the line in the line chart.
+          // TODO refactor this so the color is a model property
+          // set by the main program.
+          d3.select(this)
+            .style("stroke", "#FF8800")
+            .style("stroke-width", "1px");
+
+          model.hoveredData = [d.id];
         })
         .on("mousemove", function () {
           tooltip
@@ -196,12 +223,18 @@ define(["model", "d3", "topojson"], function (Model, d3, topojson) {
         })
         .on("mouseout", function () {
           tooltip.style("visibility", "hidden");
+          model.hoveredData = [];
+          d3.select(this)
+            .style("stroke", "gray")
+            .style("stroke-width", "0.1px");
         });
     });
 
     function countries(countryFeatures){
       var countries = countriesG.selectAll("path").data(countryFeatures);
-      countries.enter().append("path");
+      countries.enter().append("path")
+        .style("stroke", "gray")
+        .style("stroke-width", "0.1px");
       return countries;
     }
 
--- examples/d3LinkedChoropleth/main.js
+++ examples/d3LinkedChoropleth/main.js
@@ -3,21 +3,21 @@
 //  * use box instead of size
 //  * grab UN population data
 //  * grab natural earth country boundaries
-require(['d3', 'choropleth', 'lineChart'], function (d3, Choropleth, LineChart) {
-  var div = document.getElementById('container'),
+require(["d3", "choropleth", "lineChart"], function (d3, Choropleth, LineChart) {
+  var div = document.getElementById("container"),
       choropleth = Choropleth(div),
       lineChart = LineChart(div);
 
   choropleth.set({
-    idField: 'country',
-    colorField: 'population'
+    idField: "country",
+    colorField: "population"
   });
 
   lineChart.set({
-    xField: 'date',
-    yField: 'population',
-    idField: 'country',
-    yLabel: 'Population (bn)',
+    xField: "date",
+    yField: "population",
+    idField: "country",
+    yLabel: "Population (bn)",
     margin: {
       top: 20,
       right: 20,
@@ -26,33 +26,33 @@ require(['d3', 'choropleth', 'lineChart'], function (d3, Choropleth, LineChart) 
     }
   });
 
-  d3.json('countries.json', function (err, countries) {
+  d3.json("countries.json", function (err, countries) {
     var naturalEarthToUnitedNations = {
-          'United States': 'United States of America',
-          'Solomon Is.': 'Solomon Islands',
-          'Russia': 'Russian Federation',
-          'Iran': 'Iran (Islamic Republic of)',
-          'Dem. Rep. Congo': 'Democratic Republic of the Congo',
-          'Venezuela': 'Venezuela (Bolivarian Republic of)',
-          'Dominican Rep.': 'Dominican Republic',
-          'Bolivia': 'Bolivia (Plurinational State of)',
-          'Côte d\'Ivoire': 'C�te d\'Ivoire',
-          'Tanzania': 'United Republic of Tanzania',
-          'Central African Rep.': 'Central African Republic',
-          'S. Sudan': 'South Sudan',
-          'Eq. Guinea': 'Equatorial Guinea',
-          'W. Sahara': 'Western Sahara',
-          'Syria': 'Syrian Arab Republic',
-          'Vietnam': 'Viet Nam',
-          'Czech Rep.': 'Czech Republic',
-          'Bosnia and Herz.': 'Bosnia and Herzegovina',
-          'Macedonia': 'TFYR Macedonia',
-          'Moldova': 'Republic of Moldova',
-          'Palestine': 'State of Palestine',
-          'Lao PDR': 'Lao People\'s Democratic Republic',
-          'Dem. Rep. Korea': 'Dem. People\'s Republic of Korea',
-          'Korea': 'Republic of Korea',
-          'Brunei': 'Brunei Darussalam'
+          "United States": "United States of America",
+          "Solomon Is.": "Solomon Islands",
+          "Russia": "Russian Federation",
+          "Iran": "Iran (Islamic Republic of)",
+          "Dem. Rep. Congo": "Democratic Republic of the Congo",
+          "Venezuela": "Venezuela (Bolivarian Republic of)",
+          "Dominican Rep.": "Dominican Republic",
+          "Bolivia": "Bolivia (Plurinational State of)",
+          "Côte d\"Ivoire": "C�te d\"Ivoire",
+          "Tanzania": "United Republic of Tanzania",
+          "Central African Rep.": "Central African Republic",
+          "S. Sudan": "South Sudan",
+          "Eq. Guinea": "Equatorial Guinea",
+          "W. Sahara": "Western Sahara",
+          "Syria": "Syrian Arab Republic",
+          "Vietnam": "Viet Nam",
+          "Czech Rep.": "Czech Republic",
+          "Bosnia and Herz.": "Bosnia and Herzegovina",
+          "Macedonia": "TFYR Macedonia",
+          "Moldova": "Republic of Moldova",
+          "Palestine": "State of Palestine",
+          "Lao PDR": "Lao People\"s Democratic Republic",
+          "Dem. Rep. Korea": "Dem. People\"s Republic of Korea",
+          "Korea": "Republic of Korea",
+          "Brunei": "Brunei Darussalam"
         },
         geoms = countries.objects.countriesGeoJSON.geometries;
     geoms.forEach(function (d) {
@@ -62,16 +62,16 @@ require(['d3', 'choropleth', 'lineChart'], function (d3, Choropleth, LineChart) 
     choropleth.countries = countries;
   });
 
-  d3.csv('un_locations.csv', function (error, locations) {
+  d3.csv("un_locations.csv", function (error, locations) {
     var countryNamesByCode = {}
 
     locations.forEach(function (d) {
-      if(d.locationTypeName == 'Country/Area'){
+      if(d.locationTypeName == "Country/Area"){
         countryNamesByCode[d.unCountryCode] = d.countryName;
       }
     });
 
-    d3.csv('un_population.csv', function(error, data) {
+    d3.csv("un_population.csv", function(error, data) {
       data = data.filter(function (d) {
         d.date = new Date(d.year, 0);
         d.population = (+d.population) / 1000000;
@@ -85,13 +85,13 @@ require(['d3', 'choropleth', 'lineChart'], function (d3, Choropleth, LineChart) 
         return d.date.getFullYear();
       });
 
-      lineChart.when('selectedYear', function (selectedYear) {
+      lineChart.when("selectedYear", function (selectedYear) {
         choropleth.data = data.filter(function (d) {
           return d.date.getFullYear() == selectedYear;
         });
       });
 
-      choropleth.when('visibleRegions', function (visibleRegions) {
+      choropleth.when("visibleRegions", function (visibleRegions) {
         var visibleCountries = {};
         visibleRegions.forEach(function (d) {
           visibleCountries[d.id] = true;
@@ -100,11 +100,15 @@ require(['d3', 'choropleth', 'lineChart'], function (d3, Choropleth, LineChart) 
           return visibleCountries[d.country];
         });
       });
+
+      choropleth.when("hoveredData", function (hoveredData) {
+        lineChart.highlightedData = hoveredData;
+      });
     });
   });
 
   setSizes();
-  window.addEventListener('resize', setSizes);
+  window.addEventListener("resize", setSizes);
   function setSizes(){
     var ratio = 2 / 3;
     choropleth.box = {
